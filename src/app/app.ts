32d24f89468fbd1ef462e724d0bb15efbe122fcf
João Vitor Santana <94786847+jvs-dev@users.noreply.github.com>
2025-09-12 16:42:44 -0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyComponent } from './my-component/my-component';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyComponent, Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('testAngular2');
